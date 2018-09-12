b09db777098e9da0a31d39cabd117309e74a5805
Murray <murrayhoggett@gmail.com>
2018-09-12 15:15:12 +0100

    Fixed a bug in extract so it is now in 6.25 m increments (bug was in the _geom files), and added velocity file writing and plotting.

--- vel_extract.sh
+++ vel_extract.sh
@@ -4,4 +4,6 @@ bathyfile=jc007_EM120_and_bathy13-14.grd
 coords=L6a.26.su_geom
 stem=`echo $bathyfile | tr -d .grd`
 
-grdtrack $coords -G$bathyfile > ${stem}_profile.txt
+grdtrack $coords -G$bathyfile | sort -k1 | uniq > temp.txt
+sample1d temp.txt -Ar -Fl -T6.25e > ${stem}_profile.txt
+rm temp.txt
--- vel_model.py
+++ vel_model.py
@@ -4,11 +4,11 @@ Created on Tue Sep 11 20:09:37 2018
 
 @author: murray
 """
+
 # Global vars 
 watervel = 1500.0
-num_t_samples = 6000
-num_shotpoints = 7000
-
+num_t_samples = 10000 # should be 19000, given a 0.0002 second sample rate and 19s seis line.  
+bathyfilename="jc007_EM120_an_bathy13-14_profile.txt"
 
 # Import dependencies 
 import numpy as np 
@@ -16,8 +16,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Make fake linear seafloor. Will replace with real seafloor later. 
-seaf = np.linspace(2000,2200, num_shotpoints)
+#seaf = np.linspace(2000,2200, num_shotpoints)
+seaf = np.genfromtxt(fname=bathyfilename)
 seaf = seaf.astype(int)
+num_shotpoints = seaf.shape[0]
 
 # make matrix to hold vel model.
 mat = np.ones((num_t_samples, num_shotpoints))
@@ -29,12 +31,20 @@ func[700:] = np.linspace(6000, 8000,len(func) - 700)
 func = pd.rolling_mean(func, 101, min_periods=1)
 
 # Build matrix
+print("Building velocity model....")
 for i in range(mat.shape[1]):    
-    mat[0:seaf[i], i] = watervel
-    mat[seaf[i]:seaf[i]+len(func), i] = func
-    mat[seaf[i]+len(func):-1, i] = np.max(func)
-        
+    mat[0:seaf[i,2], i] = watervel
+    mat[seaf[i,2]:seaf[i,2]+len(func), i] = func
+    mat[seaf[i,2]+len(func):-1, i] = np.max(func)
+
+print(mat.shape)
+#np.savetxt(fname="vel_model.txt", X=mat)
+print("Finished building model. Making fig...")
+
 # Plot matrix to sanity check
+plt.figure(figsize=(5, 8), dpi=50, facecolor='w', edgecolor='k')
 plt.imshow(mat)
 plt.colorbar()
+plt.tight_layout()
+plt.savefig("vel_model.jpg")
 plt.show()
