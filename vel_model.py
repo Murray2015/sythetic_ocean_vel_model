4ffcd6c73e7b9ee01a599b05ed0d15d2f1c895aa
Murray Hoggett <murrayhoggett@gmail.com>
2018-09-12 07:49:51 +0100

    Added 2 second velocity model

--- vel_model.py
+++ vel_model.py
@@ -4,35 +4,37 @@ Created on Tue Sep 11 20:09:37 2018
 
 @author: murray
 """
+# Global vars 
+watervel = 1500.0
+num_t_samples = 6000
+num_shotpoints = 7000
 
+
+# Import dependencies 
 import numpy as np 
 import matplotlib.pyplot as plt
+import pandas as pd
 
-watervel = 1500.0
-num_t_samples = 4000
-num_shotpoints = 4000
-
-
+# Make fake linear seafloor. Will replace with real seafloor later. 
 seaf = np.linspace(2000,2200, num_shotpoints)
 seaf = seaf.astype(int)
-mat = np.ones((num_t_samples, num_shotpoints))
-func = 4000 + np.arange(500)*500
 
+# make matrix to hold vel model.
+mat = np.ones((num_t_samples, num_shotpoints))
+
+# Define seafloor velocity profile 
+func = np.ones((2000))
+func[0:700] = np.linspace(watervel, 6000,700)
+func[700:] = np.linspace(6000, 8000,len(func) - 700)
+func = pd.rolling_mean(func, 101, min_periods=1)
+
+# Build matrix
 for i in range(mat.shape[1]):    
     mat[0:seaf[i], i] = watervel
     mat[seaf[i]:seaf[i]+len(func), i] = func
     mat[seaf[i]+len(func):-1, i] = np.max(func)
         
-
+# Plot matrix to sanity check
 plt.imshow(mat)
 plt.colorbar()
-plt.show()
-
-mat.shape
-
-
-
-temp = np.ones((40,200))
-temp[1,199]
-plt.imshow(temp)
 plt.show()
