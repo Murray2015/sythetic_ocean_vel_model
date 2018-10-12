e9793612689259f428ba2f01f3aaaec1883bdd32
Murray <murrayhoggett@gmail.com>
2018-10-12 12:54:53 +0100

    Fixed vel model. Now hopefully finished.

--- vel_model.py
+++ vel_model.py
@@ -6,8 +6,6 @@ Created on Tue Sep 11 20:09:37 2018
 """
 
 
-### Currently not quite working - need to sort out the math of making the matrix, considering a sample rate and a given time length for the record. Have now implemented changing seafloor depth into twtt, but not necessarily got the math correct for indexing into the "mat" matrix of the velocity model. Might need to handwrite in pseudocode, but it is most of the way there... 
-
 # Global vars 
 watervel = 1500.0
 time_sample_rate = 0.001
@@ -24,11 +22,10 @@ import pandas as pd
 seaf = np.genfromtxt(fname=bathyfilename)
 seaf_t = 2*(abs(seaf[:,2])/1500.0)
 seaf = np.append(seaf, np.reshape(seaf_t, (seaf_t.shape[0], 1)), axis=1) # Convert distance into twtt in column [,2] 
-seaf = seaf.astype(int)
 num_shotpoints = seaf.shape[0]
 
 # In future, change this for sample rate 
-num_t_samples = record_length_s / time_sample_rate  # should be 19000, given a 0.0002 second sample rate and 19s seis line. 
+num_t_samples = 8000 #record_length_s / time_sample_rate  # should be 19000, given a 0.0002 second sample rate and 19s seis line. 
 
 # make matrix to hold vel model.
 mat = np.ones((num_t_samples, num_shotpoints))
@@ -47,19 +44,25 @@ print(func)
 print(seaf)
 # Build matrix
 print("Building velocity model....")
-for i in range(mat.shape[1]):    
-    mat[0:seaf[i,3], i] = watervel
-    mat[seaf[i,3]:seaf[i,3]+len(func), i] = func
-    mat[seaf[i,3]+len(func):-1, i] = np.max(func)
+# For each seismic trace 
+for i in range(mat.shape[1]):   
+    # Find the seafloor time in terms of the sample rate
+    seaf_time_in_samprate = int(seaf[i,3] / time_sample_rate)
+    mat[0:seaf_time_in_samprate, i] = watervel
+    mat[seaf_time_in_samprate:seaf_time_in_samprate+len(func), i] = func
+    mat[seaf_time_in_samprate+len(func):-1, i] = np.max(func)
 
 print(mat.shape)
-#np.savetxt(fname="vel_model.txt", X=mat)
+np.savetxt(fname="vel_model.txt", X=mat)
 print("Finished building model. Making fig...")
 
 # Plot matrix to sanity check
-plt.figure(figsize=(5, 8), dpi=50, facecolor='w', edgecolor='k')
-plt.imshow(mat)
-plt.colorbar()
+plt.figure(figsize=(16, 8), dpi=50, facecolor='w', edgecolor='k')
+plt.imshow(mat, aspect="auto", cmap="inferno")
+plt.colorbar().set_label("Seismic velocity (m/s)")
+plt.contour(mat, colors="black")
+plt.xlabel("Trace number from 0")
+plt.ylabel("Time sample")
 plt.tight_layout()
-plt.savefig("vel_model.jpg")
+#plt.savefig("vel_model.jpg")
 plt.show()
